ef22a7fcc4bf252d13ae8764e651a0ca917fbd34
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-10 22:15:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,3 +30,11 @@ def bulk_tweet_shortener(array_of_tweets)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    return word_substituter(tweet)
+  else
+    tweet
+  end
+  
